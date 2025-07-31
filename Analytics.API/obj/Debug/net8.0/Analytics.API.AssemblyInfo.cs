49395cc3d36266a280366213f4f79a0c42b0f104
mateusgomst <mateusgomes9119@gmail.com>
2025-07-31 15:23:14 -0300

    feat: Add Dockerfile and docker-compose for Analytics API setup

--- Analytics.API/obj/Debug/net8.0/Analytics.API.AssemblyInfo.cs
+++ Analytics.API/obj/Debug/net8.0/Analytics.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Analytics.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c2a710c91efd87ee61946365d4a93331cdbd3e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Analytics.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Analytics.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
